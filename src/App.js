ce12d3115f91cc5d5a7d07f30732609b3c001ee0
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-03-05 22:31:25 +0530

    modified file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   let products = [
     { id: 1, name: "Product 1", price: 440, image: "./coffee1.jpg" },
     { id: 2, name: "Product 2", price: 560, image: "./coffee2.jpg" },
-    { id: 3, name: "Product 3", price: 500, image: "./coffee2.jpg" },
+    { id: 3, name: "Product 3", price: 500, image: "./coffee3.jpg" },
     { id: 4, name: "Product 4", price: 100, image: "./coffee4.jpg" },
     { id: 5, name: "Product 5", price: 900, image: "./coffee5.jpg" },
     { id: 6, name: "Product 6", price: 900, image: "./coffee1.jpg"},
@@ -17,10 +17,11 @@ function App() {
       {products.map((e) => (
         <div key={e.id}>
           <div>
-            <img src={e.image} alt={e.name} />
+            <img src={e.image} alt={e.name} width={100} height={100}/>
           </div>
           <div>{e.name}</div>
           <div>{e.price}</div>
+          <div><button>Add</button></div> 
         </div>
       ))}
     </div>
