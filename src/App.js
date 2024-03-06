e856f02a4acfe3b67674cca915c575fd04fcb393
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-03-06 13:59:26 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   let products = [
     { id: 1, name: "Product 1 changed", price: 440, image: "./coffee1.jpg" },
-    { id: 2, name: "Product 2", price: 560, image: "./coffee2.jpg" },
+    { id: 2, name: "Product 2 changed", price: 560, image: "./coffee2.jpg" },
     { id: 3, name: "Product 3", price: 500, image: "./coffee3.jpg" },
     { id: 4, name: "Product 4", price: 100, image: "./coffee4.jpg" },
     { id: 5, name: "Product 5", price: 900, image: "./coffee5.jpg" },
