e2c8131977fa442e2ebc20c365a8a8193b3458b1
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-03-07 08:08:41 +0530

    Updated file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react'
 import './App.css'
+import { Navbar } from './Components/Navbar/Navbar';
 
 function App() {
 
-  let products = [
-    { id: 1, name: "Product 1 changed", price: 440, image: "./coffee1.jpg" },
-    { id: 2, name: "Product 2 changed", price: 560, image: "./coffee2.jpg" },
-    { id: 3, name: "Product 3", price: 500, image: "./coffee3.jpg" },
-    { id: 4, name: "Product 4", price: 100, image: "./coffee4.jpg" },
-    { id: 5, name: "Product 5", price: 900, image: "./coffee5.jpg" },
-    { id: 6, name: "Product 6", price: 900, image: "./coffee1.jpg"},
-  ];
+
   return (
-    <div className='App-main'>
-      <h1 className='App-h'>Coffee shop</h1><br/>
-      {products.map((e) => (
-        <div className='App-container' key={e.id}>
-          <div>
-            <img src={e.image} alt={e.name} width={200} height={200}/>
-          </div>
-          <div >{e.price}</div>
-          <div><button>Add</button></div> 
-        </div>
-      ))}
+    <div >
+      <Navbar/>
     </div>
   );
 }
